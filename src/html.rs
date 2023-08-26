597414a20108ec169069481e14405fa60b612bec
Alexander Alekhin <sphrand@gmail.com>
2023-08-26 19:39:27 +0300

    Update: Function arguments

--- src/html.rs
+++ src/html.rs
@@ -1,29 +1,24 @@
 pub fn escape(text: String) -> String {
-    text.replace("&", "&amp;")
-        .replace("\"", "&quot;")
-        .replace("<", "&lt;")
-        .replace(">", "&gt;")
+    text.replace('&', "&amp;")
+        .replace('\"', "&quot;")
+        .replace('<', "&lt;")
+        .replace('>', "&gt;")
 }
 
-#[cfg(test)]
-mod tests {
-    use super::escape;
-
-    #[test]
-    fn escape_all() {
-        assert_eq!(escape("foo-bar".to_string()), "foo-bar".to_string());
-        assert_eq!(escape("foo\"bar".to_string()), "foo&quot;bar".to_string());
-        assert_eq!(
-            escape("<foo\"bar".to_string()),
-            "&lt;foo&quot;bar".to_string()
-        );
-        assert_eq!(
-            escape("<foo\"bar>".to_string()),
-            "&lt;foo&quot;bar&gt;".to_string()
-        );
-        assert_eq!(
-            escape("<&foo\"bar>".to_string()),
-            "&lt;&amp;foo&quot;bar&gt;".to_string()
-        );
-    }
+#[test]
+fn escape_all() {
+    assert_eq!(escape("foo-bar".to_string()), "foo-bar".to_string());
+    assert_eq!(escape("foo\"bar".to_string()), "foo&quot;bar".to_string());
+    assert_eq!(
+        escape("<foo\"bar".to_string()),
+        "&lt;foo&quot;bar".to_string()
+    );
+    assert_eq!(
+        escape("<foo\"bar>".to_string()),
+        "&lt;foo&quot;bar&gt;".to_string()
+    );
+    assert_eq!(
+        escape("<&foo\"bar>".to_string()),
+        "&lt;&amp;foo&quot;bar&gt;".to_string()
+    );
 }
