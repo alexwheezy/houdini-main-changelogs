1ca39a0f1cc040507c2cc616bc4347cb1be187a7
Alexander Alekhin <sphrand@gmail.com>
2023-08-30 20:39:38 +0300

    update: Helper functions moved to utils module

--- src/main.rs
+++ src/main.rs
@@ -12,41 +12,15 @@ extern crate serde_json;
 use dotenv::dotenv;
 use failure::Error;
 use select::document::Document;
-
 use std::env;
-use std::fs;
 
 mod bot;
 mod html;
 mod log;
 mod parsers;
+mod utils;
 
 use crate::parsers::*;
-
-const FILE_NAME: &str = "houdini_main_changelogs.last_id";
-
-fn file_path() -> std::path::PathBuf {
-    let config_dir: Option<std::path::PathBuf> = env::var("CONFIG_DIR")
-        .ok()
-        .and_then(|value| value.parse().ok())
-        .or_else(dirs::config_dir);
-
-    config_dir.unwrap_or("/tmp/".into()).join(FILE_NAME)
-}
-
-fn read_last_id() -> Result<i32, Error> {
-    println!("Reading last-id at: {:?}", file_path().to_str());
-    let src = fs::read(file_path())?;
-    let src = String::from_utf8_lossy(&src);
-    let value: i32 = src.trim().parse()?;
-
-    Ok(value)
-}
-
-fn save_last_id(id: i32) -> std::io::Result<()> {
-    println!("Writing last-id {} to: {:?}", id, file_path().to_str());
-    fs::write(file_path(), id.to_string())
-}
 
 fn main() -> Result<(), Error> {
     env_logger::init();
@@ -56,34 +30,22 @@ fn main() -> Result<(), Error> {
         .ok()
         .expect("Expected BOT_TOKEN env var");
     let chat_id = env::var("CHAT_ID").ok().expect("Expected CHAT_ID env var");
-    let dev = env::var("DEV").ok().is_some();
 
-    let last_id = read_last_id().unwrap_or(0);
-    let mut last_id_to_be_saved = last_id;
-
-    println!("Last ID: {}", last_id);
-    println!("Starting fetch changelog list...");
-
-    //let document = Document::from(include_str!("sidefx_changelogs.html"));
+    //let html = Document::from(include_str!("sidefx_changelogs.html"));
+    //let document = Document::from(html);
     let html = reqwest::get("https://sidefx.com/changelog")?.text()?;
     let document = Document::from(html.as_str());
-    let change_log_list = parse_change_log(&document, last_id)?;
+    let mut changelog = parse_change_log(&document)?;
 
-    let (build, change_log) = change_log_list.last_log();
-    let publish = format!("<b>Daily Build: {build}</b>\n\n{change_log}");
-    //println!("{publish}");
+    changelog.update().unwrap();
+    changelog.store().unwrap();
 
-    let bot = bot::Bot::new(bot_token);
-    bot.send_message(&chat_id.clone(), &publish)?;
+    let (build, changelog) = changelog.last_record().unwrap();
+    let publish = format!("<b>Daily Build: {build}</b>\n\n{changelog}");
+    println!("{publish}");
 
-    //if id > last_id_to_be_saved {
-    //    last_id_to_be_saved = id;
-    //}
-
-    //if !dev {
-    //    println!("Last ID: {}", last_id_to_be_saved);
-    //    save_last_id(last_id_to_be_saved)?;
-    //}
+    //let bot = bot::Bot::new(bot_token);
+    //bot.send_message(&chat_id.clone(), &publish)?;
 
     Ok(())
 }
